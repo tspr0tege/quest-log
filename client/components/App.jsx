ff76c8b6f03e6ad66ae29819d06ce85034df9894
Squall Leonhart <squallpl1983@gmail.com>
2021-11-17 18:00:29 -0500

    Very basic server and React structure created

---
+++ client/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return(
+      <>
+        <p>App.jsx has been loaded</p>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const BUILD_DIR = path.resolve(__dirname, './public/build');
+const APP_DIR = path.resolve(__dirname, './client');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: APP_DIR + '/index.js'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: BUILD_DIR
+  },
+  module: {
+    rules: [
+      // {
+      //   test: /\.(png|jpe?g|gif)$/i,
+      //   use: [
+      //     {
+      //       loader: 'file-loader',
+      //     },
+      //   ],
+      // },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/react']
+          }
+        }
+      }
+    ]
+  }
+}
