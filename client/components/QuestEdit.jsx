b05a9fa7f252a0a4ece4c93eb7b9e5027d6cbaf0
Squall Leonhart <squallpl1983@gmail.com>
2021-11-30 17:44:14 -0500

    Refactored QuestEdit.jsx to use single state hook called options. resolve #15

--- client/components/QuestEdit.jsx
+++ client/components/QuestEdit.jsx
@@ -5,41 +5,80 @@ import { Context } from './App.jsx';
 
 export default ({ quest }) => {
   const { editQuest, questList } = useContext(Context);
-  let [title, setTitle] = useState(quest.title);
-  let [description, setDescription] = useState(quest.description);
-  let [parentQuest, setParent] = useState(quest.parentQuest);
-  let [contribution, setContribution] = useState(quest.contribution);
-  // event.persist(), state = {title, description, parent}
+  const [options, setOptions] = useState({
+    title: quest.title,
+    description: quest.description,
+    parentQuest: quest.parentQuest,
+    contribution: quest.contribution
+  });
 
   const submit = (e) => {
     e.preventDefault();
-    // let { title, description, parentQuest } = e.target.form;
-    editQuest(quest, { title, description, parentQuest, contribution });
+    editQuest(quest, options);
+  }
+
+  const updateTitle = (e) => {
+    e.persist();
+    setOptions({
+      ...options,
+      title: e.target.value
+    });
+  }
+
+  const updateDescription = (e) => {
+    e.persist();
+    setOptions({
+      ...options,
+      description: e.target.value
+    });
   }
 
   const updateParent = (e) => {
-    console.log(e.target.value);
+    e.persist();
+    let newParent;
     if (e.target.value === 'None') {
-      setParent(null);
+      newParent = null;
     } else {
+      // traverses depth as a map, through children quests, to find new parent
       let depth = e.target.options[e.target.selectedIndex].dataset.depth.split('-');
-      let newParent = questList[depth[0]];
+      newParent = questList[depth[0]];
       for (let i = 1; i < depth.length; i++) {
         newParent = newParent.subQuests[depth[i]];
       }
-      setParent(newParent);
     }
+    setOptions({
+      ...options,
+      parentQuest: newParent
+    });
+  }
+
+  const updateContribution = (e) => {
+    e.persist();
+    setOptions({
+      ...options,
+      contribution: parseInt(e.target.value)
+    });
   }
 
   return (
     <form id='quest-edit-form'>
       <div>
         <h3>Title:</h3>
-        <input type='text' value={title} onChange={e => {setTitle(e.target.value)}}></input>
+        <input 
+          type='text' 
+          value={options.title} 
+          onChange={updateTitle}>          
+        </input>
       </div>
       <div>
         <h3>Description:</h3>
-        <textarea value={description} onChange={e => {setDescription(e.target.value)}} placeholder='Enter a description...' rows='5' style={{width: '95%'}}></textarea>
+        <textarea 
+          value={options.description} 
+          onChange={updateDescription} 
+          placeholder='Enter a description...' 
+          rows='5' 
+          style={{width: '95%'}}>
+        </textarea>
       </div>
       <div>
         <h3>Progress:</h3>
@@ -52,18 +91,29 @@ export default ({ quest }) => {
       <div>
         <h3>Parent Quest Line:</h3>
         <select
-        id="parent-quest-dropdown" 
-        onChange={updateParent}
-        defaultValue={(parentQuest) ? parentQuest.id : null}>
+          id="parent-quest-dropdown" 
+          onChange={updateParent}
+          defaultValue={(options.parentQuest) ? options.parentQuest.id : null}>
+
           <option value={null}>None</option>
           {dropList(quest)}
         </select>
       </div>
-      {parentQuest && <div>
+      {options.parentQuest && <div>
         <h3>Contribution to Quest Line:</h3>
         <div style={{display: 'flex'}}>
-          <input type='range' min='0' max='100' step='1' value={contribution} onChange={e => {setContribution(parseInt(e.target.value))}}/>
-          <input type='number' min='0' max='100' value={contribution} onChange={e => {setContribution(parseInt(e.target.value))}}/>
+          <input 
+            type='range' 
+            min='0' max='100' step='1' 
+            value={options.contribution} 
+            onChange={updateContribution}/>
+
+          <input 
+            type='number' 
+            min='0' max='100' 
+            value={options.contribution} 
+            onChange={updateContribution}/>
+
         </div>
       </div>}
       <button onClick={submit} style={{marginTop: '20px'}}>Submit Changes</button>
@@ -74,10 +124,12 @@ export default ({ quest }) => {
 const dropList = (targetQuest) => {
   const { questList } = useContext(Context);
 
+  // List of quests (self and children) that would be invalid parents
   let skipList = targetQuest.linearMapAll((quest) => {
     return quest;
   });
 
+  // Returns all quests that are not in skip list, from App.state object
   const fullList = questList.map((quest, index) => {
     return quest.linearMapAll((q, depth) => {      
       let key = depth.join('-');
@@ -86,6 +138,5 @@ const dropList = (targetQuest) => {
         }
     }, [index]);
   });
-
   return fullList;
 }
