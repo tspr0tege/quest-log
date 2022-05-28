0f1323bd072afbdf99eebecc5b7081d6d0b9d65f
Squall Leonhart <squallpl1983@gmail.com>
2022-05-28 18:30:48 -0400

    Updated sequelize connection to use env variable

--- server/util/database.js
+++ server/util/database.js
@@ -4,12 +4,13 @@ const Sequelize = require('sequelize');
 
 const {
   PG_USER,
-  PG_PASS
+  PG_PASS,
+  DATABASE_URL
 } = process.env;
 
 // const sequelize = new Sequelize(database, username, password, options);
 const sequelize = new Sequelize('quest-log', PG_USER, PG_PASS, {
-  // host: '',
+  host: DATABASE_URL,
   // port: 0,
   // username: '',
   // password: '',
