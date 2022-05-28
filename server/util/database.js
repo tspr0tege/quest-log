40ed5cc2c2a4c4de0c6d6b86bef5885381bfc83a
Squall Leonhart <squallpl1983@gmail.com>
2022-05-28 18:51:09 -0400

    Updated env

--- server/util/database.js
+++ server/util/database.js
@@ -5,19 +5,16 @@ const Sequelize = require('sequelize');
 const {
   PG_USER,
   PG_PASS,
-  DATABASE_URL
+  DATABASE_URL,
+  DATABASE_PORT,
+  DATABASE_NAME
 } = process.env;
 
 // const sequelize = new Sequelize(database, username, password, options);
-const sequelize = new Sequelize('quest-log', PG_USER, PG_PASS, {
+const sequelize = new Sequelize(DATABASE_NAME, PG_USER, PG_PASS, {
   host: DATABASE_URL,
-  // port: 0,
-  // username: '',
-  // password: '',
-  // database: '',
-  dialect: 'postgres',
-  // dialectModule: '',
-  // dialectModulePath: ''
+  port: DATABASE_PORT,
+  dialect: 'postgres'
 });
 
 module.exports = sequelize;
