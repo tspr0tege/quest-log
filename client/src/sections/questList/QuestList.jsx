115a73e266019667feb57e65c27daa499982a0a4
Squall Leonhart <squallpl1983@gmail.com>
2022-06-02 18:23:52 -0400

    Questlist made scrollable

--- client/src/sections/questList/QuestList.jsx
+++ client/src/sections/questList/QuestList.jsx
@@ -7,14 +7,16 @@ import './QuestList.css'
 export default ({ quests, handleClick, completeQuest }) => {
 
   return (
-    <ul>
-      {quests.map((quest, i) => {
-        return(
-          <li key={i}>
-            <QuestListItem quest={quest} handleClick={handleClick} completeQuest={completeQuest}/>
-          </li>
-        );
-      })}
-    </ul>
+    <div className="quest-list">
+      <ul>
+        {quests.map((quest, i) => {
+          return(
+            <li key={i}>
+              <QuestListItem quest={quest} handleClick={handleClick} completeQuest={completeQuest}/>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
--- server/util/database.js
+++ server/util/database.js
@@ -3,11 +3,7 @@ const Sequelize = require('sequelize');
 // const { ModuleFilenameHelpers } = require('webpack');
 
 const {
-  PG_USER,
-  PG_PASS,
-  DATABASE_URL,
-  DATABASE_PORT,
-  DATABASE_NAME
+  DATABASE_URL
 } = process.env;
 
 // const sequelize = new Sequelize(database, username, password, options);
