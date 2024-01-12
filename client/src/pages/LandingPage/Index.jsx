d53bb449e3f03d66c12d07df3520fc6f21a4a86d
Squall Leonhart <squallpl1983@gmail.com>
2024-01-11 23:31:36 -0600

    Landing page created
    -Webpack config reverted. Issue was not with pathing, but naming
    convention casing. Currently using the default "index" instead of named
    jsx files and letting the transpiler auto-detect. It was looking for
    lower case and I had upper, which was causing a caching issue with
    bundle.js
    
    -Landing page to be remade later with a lighter background similar to
    Figma Landing page concepts ExpressVPN style. Planning to use Dall-e to
    generate a multi-layered background in this style.
    
    -Consider changing font-style to a sans style.

--- client/src/pages/LandingPage/Index.jsx
+++ client/src/pages/LandingPage/Index.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 
+import './LandingPage.css';
+
+const temporaryButtonStyle = {
+  fontWeight: '600',
+  padding: '15px 30px',
+  fontSize: '30px',
+  boxSizing: 'content-box',
+  borderRadius: '50px',
+  margin: '60px',
+  backgroundColor: 'var(--goldBrown)',
+};
 
 export default () => (
-  <>
-    <h1>WELCOME</h1>
-  </>
+  <div id='landing-page'>
+    <div>
+      <h1>A Personal Productivity App for gamers</h1>
+      <p>Experience your life as an adventure!</p>
+      {/* Create a reuseable button component */}
+      <button style={temporaryButtonStyle}>Login/Sign-up</button>
+    </div>
+    <img src='https://i.ibb.co/VgHTkTg/bg-dark-torch.jpg' />
+  </div>  
 )
